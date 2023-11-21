2f5bd24aa2a5869c78d5a684c383c3afaba6c6a0
Bhargavakkeni <bhargavakkeni123@gmail.com>
2023-11-21 12:18:23 +0530

    removed home from urls

--- ProjectsOfDjango/ProjectsOfDjango/urls.py
+++ ProjectsOfDjango/ProjectsOfDjango/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',include('homeapp.urls')),
+    path('',include('homeapp.urls')),
     path('omsapp/',include('loginapp.urls')),
     path('calculatorapp/',include('calculatorapp.urls')),
     path('pollingapp/',include('pollingapp.urls')),
